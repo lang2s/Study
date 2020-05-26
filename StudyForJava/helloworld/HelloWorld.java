2371a173db1bbf5ba7e8b0ca0534ef0e6efee261
lang2s <lang2s@naver.com>
2020-05-26 16:12:01 +0900

    java

---
+++ StudyForJava/helloworld/HelloWorld.java
@@ -0,0 +1,8 @@
+package StudyForJava.helloworld;
+
+public class HelloWorld {
+    
+    public static void main(String[] args){
+        System.out.println("helloworld");
+    }
+}
---
+++ StudyForJava/helloworld/StringApp.java
@@ -0,0 +1,17 @@
+package StudyForJava.helloworld;
+
+public class StringApp {
+    public static void main(String[] args) {
+      System.out.println("Hello World"); // String
+      System.out.println('H');  // Character
+      System.out.println("H"); // String
+
+      System.out.println("Hello World");
+    
+      // new line
+      System.out.println("Hello \nWorld");
+
+      // escape
+      System.out.println("Hello \"world\"");
+    }
+}
