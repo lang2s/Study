0f433c518f74a403d5082a36b728c6f30bcefbe3
lang2s <lang2s@naver.com>
2020-05-30 17:15:56 +0900

    생활코딩

---
+++ StudyForJava/helloworld/StringOperation.java
@@ -0,0 +1,10 @@
+package StudyForJava.helloworld;
+
+public class StringOperation {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World".length()); // 11
+        System.out.println("Hello, [[name]] ... bye ".replace("[[name]]", "egoing"));
+
+    }
+}
